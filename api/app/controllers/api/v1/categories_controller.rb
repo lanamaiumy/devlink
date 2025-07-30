ad7279e73edd777fcc59450a8697a082d0c73d79
lanamaiumy <alana.maiumy@gmail.com>
2025-07-30 15:01:44 -0300

    cors.rb for Netlify deploy

--- api/app/controllers/api/v1/categories_controller.rb
+++ api/app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
   def index
-    # v1.0.1 - forçando atualização render
     @categories = Category.includes(:links).order(:name)
 
     render json: @categories.as_json(include: { links: { except: [:category_id] } })
--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:4200"
-    resource "*",
+    origins 'http://localhost:4200', 'https://golden-hummingbird-a98d96.netlify.app'
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
