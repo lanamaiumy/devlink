8adf2913e20987b452d9b603ec0ab96f7d0d41ee
lanamaiumy <alana.maiumy@gmail.com>
2025-07-31 16:07:06 -0300

    .cors permission

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -1,8 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200', 'https://golden-hummingbird-a98d96.netlify.app'
+    origins 'http://localhost:4200', 'https://devlinkhub.netlify.app'
+
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false
   end
 end
